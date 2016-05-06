e33a9bcc4c495182129b129c3b903e4d1463c68c
Stanley Hum <r.stanley.hum@gmail.com>
2016-05-05 20:11:41 -0400

    added Stores, initialState, AppReducers, overall Article Reducer

--- HandbookApp/HandbookApp/App.cs
+++ HandbookApp/HandbookApp/App.cs
@@ -1,23 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿//
+//  Copyright 2016  R. Stanley Hum <r.stanley.hum@gmail.com>
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
 
+using System.Collections.Immutable;
+
+using Redux;
 using Xamarin.Forms;
+
+using HandbookApp.States;
+using HandbookApp.Reducers;
+
 
 namespace HandbookApp
 {
     public class App : Application
     {
+        public static IStore<AppState> Store { get; private set; }
+
+
         public App()
         {
+            var initialState = new AppState {
+                Articles = ImmutableDictionary<string, Article>.Empty
+            };
+
+            Store = new Store<AppState>(ApplicationReducers.ReduceApplication, initialState);
+
             // The root page of your application
             MainPage = new ContentPage {
                 Content = new StackLayout {
                     VerticalOptions = LayoutOptions.Center,
                     Children = {
                         new Label {
-                            XAlign = TextAlignment.Center,
+                            HorizontalTextAlignment = TextAlignment.Center,
                             Text = "Welcome to Xamarin Forms!"
                         }
                     }
---
+++ HandbookApp/HandbookApp/Reducers/ArticleReducers.cs
@@ -0,0 +1,68 @@
+﻿//
+//  Copyright 2016  R. Stanley Hum <r.stanley.hum@gmail.com>
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+
+using System.Collections.Immutable;
+
+using HandbookApp.Actions;
+using HandbookApp.States;
+using Redux;
+
+namespace HandbookApp.Reducers
+{
+
+    public static class ArticleReducers
+    {
+
+
+        public static ImmutableDictionary<string, Article> AddArticleReducer(ImmutableDictionary<string, Article> previousState, AddArticleAction action)
+        {
+            if(previousState.ContainsKey(action.ArticleID))
+            {
+                return previousState.Add(action.ArticleID, new Article { ArticleId = action.ArticleID, Title = action.Title, Content = action.Content });
+            }
+
+            return previousState;
+        }
+
+
+        public static ImmutableDictionary<string, Article> DeleteArticleReducer(ImmutableDictionary<string, Article> previousState, DeleteArticleAction action)
+        {
+            if(previousState.ContainsKey(action.ArticleID))
+            {
+                return previousState.Remove(action.ArticleID);
+            }
+            
+            return previousState;
+        }
+
+
+        public static ImmutableDictionary<string, Article> ArticleReducer(ImmutableDictionary<string, Article> previousState, IAction action)
+        {
+            if (action is AddArticleAction)
+            {
+                return AddArticleReducer(previousState, (AddArticleAction)action);
+            }
+
+            if (action is DeleteArticleAction)
+            {
+                return DeleteArticleReducer(previousState, (DeleteArticleAction)action);
+            }
+
+            return previousState;
+        }
+
+    }
+}
